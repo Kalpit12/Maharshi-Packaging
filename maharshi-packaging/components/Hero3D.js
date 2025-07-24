6a1321b8fa9a8205f06c6f54e7c4aaeeaf133cc2
21CE094 Kalpit Patel <91958397+Kalpit12@users.noreply.github.com>
2025-07-24 12:25:19 +0300

    Delete maharshi-packaging directory

--- maharshi-packaging/components/Hero3D.js
+++
@@ -1,8 +0,0 @@
-// TODO: Replace with Three.js 3D box or factory flythrough
-export default function Hero3D() {
-  return (
-    <div className="w-[400px] h-[400px] bg-glass glass flex items-center justify-center">
-      <span className="text-6xl text-royal font-bold opacity-30">[3D Box]</span>
-    </div>
-  );
-} 
